102e414c066ee19dca995c35ca85aae61a543357
sinemelifhaseki <hasekisinemelif@gmail.com>
2019-12-18 16:52:46 +0300

    Updated current files with stop variable

--- artstop/wsgi.py
+++ artstop/wsgi.py
@@ -1,16 +1,16 @@
 """
-WSGI config for ArtStop project.
+WSGI config for artstop project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/dev/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ArtStop.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'artstop.settings')
 
 application = get_wsgi_application()
--- stops/models.py
+++ stops/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 
+# Create your models here.
+
 from django.contrib.gis.db import models
 
-class Location(models.Model):
+class Stop(models.Model):
     name = models.CharField(max_length=100)
     location = models.PointField()
+    category = models.CharField(max_length=50)
     address = models.CharField(max_length=100)
     city = models.CharField(max_length=50)
-
-# Create your models here.
