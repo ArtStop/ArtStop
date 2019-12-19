97da3398081040268edda8f15c402bbcd18d86d9
sinemelifhaseki <hasekisinemelif@gmail.com>
2019-12-19 23:51:16 +0300

    Added nearby artstops, nearest stops to current location

--- stops/views.py
+++ stops/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
-# Create your views here.
+# views here.
+from django.views import generic
+from django.contrib.gis.geos import fromstr
+from django.contrib.gis.db.models.functions import Distance
+from .models import Stop
+from django.contrib.gis.geos import Point
+import json, requests
+info = requests.get('http://ipinfo.io/json').json()
+result = [x.strip() for x in info['loc'].split(',')]
+latitude, longitude = result
+
+user_location = Point(float(longitude), float(latitude), srid=4326)
+
+class Home(generic.ListView):
+    model = Stop
+    context_object_name = 'stops'
+    queryset = Stop.objects.annotate(distance=Distance('location',
+    user_location)
+    ).order_by('distance')[0:6]
+    template_name = 'stops/index.html'
