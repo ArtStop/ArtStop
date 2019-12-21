2024d499eaf399a11e8865c52ab6b1df9350e516
cavitozbay <44777923+cavitozbay@users.noreply.github.com>
2019-12-22 00:15:28 +0300

    Relation model with  added

--- stops/models.py
+++ stops/models.py
@@ -3,6 +3,8 @@ from django.db import models
 # Create your models here.
 
 from django.contrib.gis.db import models
+from django.contrib.contenttypes.fields import GenericRelation
+from star_ratings.models import Rating
 
 class Stop(models.Model):
     name = models.CharField(max_length=100)
@@ -10,3 +12,7 @@ class Stop(models.Model):
     category = models.CharField(max_length=50)
     address = models.CharField(max_length=100)
     city = models.CharField(max_length=50)
+
+class RatingModel(models.Model):
+    bar = models.CharField(max_length=100)
+    ratings = GenericRelation(Rating, related_query_name='ratings')
