ce4fc932839a3f8d7fa522ababe8519b70a45a64
EmreEkici <44777170+EmreEkici@users.noreply.github.com>
2019-12-22 18:25:49 +0300

    Add files via upload

--- artstop/urls.py
+++ artstop/urls.py
@@ -24,11 +24,13 @@ from users import views as users_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', stops_views.Home.as_view(), name='home'),
-    path('profile/', users_views.profile, name='profile'),
     path('register/', users_views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     url(r'^stops/(?P<pk>[0-9]+)/$', stops_views.StopDetailView.as_view(), name='stop-detail'),
-    url(r'^ratings/', include('star_ratings.urls', namespace='ratings'))#, app_namespace='ratings'))
+    url(r'^ratings/', include('star_ratings.urls', namespace='ratings')),#, app_namespace='ratings')),
+    path('stops/<int:pk>/comment/', stops_views.add_comment_to_stop, name='add_comment_to_stop'),
+    path('comment/<int:pk>/approve/', stops_views.comment_approve, name='comment_approve'),
+    path('comment/<int:pk>/remove/', stops_views.comment_remove, name='comment_remove'),
 
 ]
