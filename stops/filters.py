47bb51047dc36b6bb5af2a2e8659204c17d0b7c0
cavitozbay <44777923+cavitozbay@users.noreply.github.com>
2019-12-22 16:46:09 +0300

    Category filter updated as Multiple Choice

--- stops/filters.py
+++ stops/filters.py
@@ -1,16 +1,17 @@
-import django_filters
-from .models import Stop
-
 class StopFilter(django_filters.FilterSet):
 
+    CHOICES = (
+            ('museum', 'Museum'),
+            ('theatre', 'Theatre'),
+            ('church', 'Church')
+        )
+    category = django_filters.ChoiceFilter(label='Category', choices=CHOICES, method='filter_for_category')
+    
     class Meta:
         model = Stop
-        fields =  {'category': ['icontains'],
+        
+        fields =  {#'category': ['icontains'],
                     'name': ['icontains']}#('category', 'name')
-    
-    """@property
-    def qs(self):
-        parent = super().qs
-        #author = getattr(self.request, 'user', None)
 
-        return parent.filter(is_published=True)"""
+    def filter_for_category(self, queryset, category, value):
+        return queryset.filter(category__icontains=value)
