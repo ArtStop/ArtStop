951bf53826ec3381020b0088a57c62d8a35c67b0
sinemelifhaseki <hasekisinemelif@gmail.com>
2019-12-22 18:17:01 +0300

    Updated stops model to add description

--- stops/models.py
+++ stops/models.py
@@ -3,8 +3,6 @@ from django.db import models
 # Create your models here.
 
 from django.contrib.gis.db import models
-from django.contrib.contenttypes.fields import GenericRelation
-from star_ratings.models import Rating
 
 class Stop(models.Model):
     name = models.CharField(max_length=100)
@@ -12,7 +10,4 @@ class Stop(models.Model):
     category = models.CharField(max_length=50)
     address = models.CharField(max_length=100)
     city = models.CharField(max_length=50)
-
-class RatingModel(models.Model):
-    bar = models.CharField(max_length=100)
-    ratings = GenericRelation(Rating, related_query_name='ratings')
+    desc = models.CharField(max_length=50)
