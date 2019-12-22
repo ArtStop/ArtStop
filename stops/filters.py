3e53114ad98509c3aab1c9d38cbd2b4725e926af
cavitozbay <44777923+cavitozbay@users.noreply.github.com>
2019-12-22 03:12:43 +0300

    filter features for stops added

---
+++ stops/filters.py
@@ -0,0 +1,15 @@
+import django_filters
+from .models import Stop
+
+class StopFilter(django_filters.FilterSet):
+
+    class Meta:
+        model = Stop
+        fields =  ['category', 'name']#('category', 'name')
+    
+    """@property
+    def qs(self):
+        parent = super().qs
+        #author = getattr(self.request, 'user', None)
+
+        return parent.filter(is_published=True)"""
